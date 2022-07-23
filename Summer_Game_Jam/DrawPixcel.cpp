9bc81ba6d1c50a7b111077febbeb779b61a9f012
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2022-07-23 15:42:46 +0900

    ウィンドウモード変更

--- Summer_Game_Jam/DrawPixcel.cpp
+++ Summer_Game_Jam/DrawPixcel.cpp
@@ -1,7 +1,10 @@
-#include "DxLib.h"
+#include"DxLib.h"
 
 int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	ChangeWindowMode(TRUE); //ウィンドウモードを有効
+	SetWindowSizeChangeEnableFlag(FALSE, FALSE); //ウィンドウを手動で変更できないようにし、ウィンドウのサイズに合わせて拡大しないようにする
+	SetWindowSize(640, 480); //画面サイズを横640、480に変更
 	if (DxLib_Init() == -1) return -1;
 
 	DrawBox(320, 240,500, 600, 0xffffff,FALSE);
