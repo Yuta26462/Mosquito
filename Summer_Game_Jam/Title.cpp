76dd2538cfa4331c5f61075c68a89d05c6460e16
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-07-24 22:27:33 +0900

    Add Selecter

--- Summer_Game_Jam/Title.cpp
+++ Summer_Game_Jam/Title.cpp
@@ -14,6 +14,26 @@ BaseScene* Title::Update() {
 	else {
 		return this;
 	}
+	if (GetSelectY() == 1 /*&& SelectNo > 0*/)SelectNo = 1;
+	if (GetSelectY() == -1 /*&& SelectNo < 3*/)SelectNo--;
+	SelectNo = 1;
+
+	if (g_KeyFlg & PAD_INPUT_A) {
+		switch (SelectNo)
+		{
+		case 0:
+			return new GameMain();
+			break;
+		/*case 1:
+			return new Result();
+			break;*/
+		case 2:
+			return new Title();
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 void Title::Draw() const {
@@ -21,4 +41,8 @@ void Title::Draw() const {
 	DrawGraph(0, 0, Title_img, FALSE);
 	DrawString(100, 400, "zキーで次のシーンへ", 0x000000);
 	DrawString(400, 400, "スペースキーで終了", 0x000000);
+	DrawCircleAA(250, 270 + SelectNo * 50, 10.2f, 3, 0xFFFFFF, TRUE);
+	DrawFormatString(200, 200, 0xFFFFFF, "%d", GetSelectX());
+	DrawFormatString(200, 250, 0xFFFFFF, "%d", GetSelectY());
+	DrawFormatString(200, 280, 0xFFFFFF, "SelectNo:%d", SelectNo);
 }
--- Summer_Game_Jam/main.cpp
+++ Summer_Game_Jam/main.cpp
@@ -12,6 +12,9 @@ int Title_img;
 int GameMain_img;
 int Result_img;
 
+int JoyPadX, JoyPadY, PadTimer;
+int SelectX, SelectY;
+
 int LoadImages();           //画像読み込み関数
 
 int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -19,7 +22,7 @@ int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     SetMainWindowText("タイトル未定");
 
     ChangeWindowMode(TRUE); //ウィンドウモードを有効
-    SetGraphMode(920, 690,16);  //解像度を決定
+    SetWindowSize(1280, 960);
     
     if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
     if (LoadImages() == -1)return -1;
@@ -33,8 +36,23 @@ int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     while (scenMG.Update() != nullptr && ProcessMessage() == 0) {
         // 入力キー取得
         g_OldKey = g_NowKey;
-        g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+        //g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+        g_NowKey = GetJoypadInputState(DX_INPUT_PAD1);
         g_KeyFlg = g_NowKey & ~g_OldKey;
+
+        GetJoypadAnalogInput(&JoyPadX, &JoyPadY, DX_INPUT_PAD1);    //アナログスティック座標を取得
+        SetJoypadDeadZone(DX_INPUT_PAD1, 0.3f);     //角度の制限を緩和
+
+        //SelectX = 0;
+        //SelectY = 0;
+
+        if (++PadTimer > 10) {
+            PadTimer = 0;
+            if (JoyPadX > 100)SelectX = 1;
+            if (JoyPadX < -100)SelectX = -1;
+            if (JoyPadY > 100)SelectY = 1;
+            if (JoyPadY < -100)SelectY = -1;
+        }
 
         ClearDrawScreen();		// 画面の初期化
 
@@ -55,4 +73,34 @@ int LoadImages() {
     if ((GameMain_img = LoadGraph("Resource/Images/background.png")) == -1)return -1;
     if ((Result_img = LoadGraph("Resource/Images/Ranking.png")) == -1)return -1;
     return 0;
+}
+
+int GetSelectX(void) {
+    if (SelectX == 1 || SelectX == -1) {
+        int Key = SelectX;
+        return Key;
+    }
+    else { return 0; }
+}
+
+int GetSelectY(void) {
+    if (SelectY == 1 || SelectY == -1) {
+        int Key = SelectY;
+        return Key;
+    }else{ return 0; }
+}
+
+int GetJoyPadX(void) { 
+    if (-1000 <= JoyPadX && JoyPadX <= 1000) {
+        int PadX = JoyPadX;
+        return PadX;
+    }else { return 0; }
+}
+
+int GetJoyPadY(void) {
+    if (-1000 <= JoyPadY && JoyPadY <= 1000) {
+        int PadY = JoyPadY;
+        return PadY;
+    }
+    else { return 0; }
 }
