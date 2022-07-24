3abc7a5d20c32937f04ea1893945ecca3c4c9ffa
Shogo-268 <syogo-yamashiro-k21h@stu.kbc.ac.jp>
2022-07-24 21:49:43 +0900

    Update "MoveEnemy"

--- Summer_Game_Jam/enemy.cpp
+++ Summer_Game_Jam/enemy.cpp
@@ -1,6 +1,10 @@
 #include "enemy.h"
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include "DxLib.h"
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 960
 
 static int timer = 0;
 
@@ -25,73 +29,46 @@ void Enemy::MoveEnemy(Enemy* enemy) {
 		enemy->CreateEnemy(enemy);
 	}
 	for (int i = 0; i < 10; i++) {
-		//if (timer % 2) {
-			if (enemy[i].flg) {
-				
-				if ((enemy[i].x <= 20 || enemy[i].x >= 620) && !enemy[i].Spawn_flg) {
-					if (enemy[i].pos <= 1) {
-						enemy[i].x += 10;
-					}
-					else
-					{
-						enemy[i].x -= 10;
-					}
-				}
-				else {
-					enemy[i].Spawn_flg = true;
-				}
 
-				if (enemy[i].Enemy_time++ % 300) {
-					enemy[i].Enemy_vector = GetRand(3);
-				}
-				if (enemy[i].Spawn_flg) {
-					switch (enemy[i].Enemy_vector) {
-					case UP:
-						enemy[i].y -= 10;
-						break;
-					case DOWN:
-						enemy[i].y += 10;
-						break;
-					case RIGHT:
-						if (enemy[i].pos <= 1) {
-							enemy[i].x += 10;
-						}
-						else
-						{
-							enemy[i].x += 5;
-						}
-						break;
-					case LEFT:
-						if (enemy[i].pos <= 1) {
-							enemy[i].x -= 5;
-						}
-						else
-						{
-							enemy[i].x -= 10;
-						}
-						break;
-					default:
-						break;
-					}
-					/*if (Enemy_vector <= 1) {
-						enemy[i].x += (GetRand(2) - 1);
-					}
-					else {
-						enemy[i].y += (GetRand(2) - 1);
-					}*/
-				}
+		//ボールの移動
+		enemy[i].x += enemy->Move_X;
+		enemy[i].y += enemy->Move_Y;
 
-				
-				
-
-				if (enemy[i].Spawn_flg && (enemy[i].x < -5 || enemy[i].x > 645 || enemy[i].y < -5 || enemy[i].y > 485)) {
-					enemy[i].flg = false;
-					enemy[i].Spawn_flg = false;
-					enemy[i].Enemy_time = 0;
-					enemy->Enemy_cnt--;
-				}
+		//壁・天井での反射
+		if (enemy[i].x < 5 || enemy[i].x > SCREEN_WIDTH - 5)      //横の壁
+		{
+			if (enemy[i].x < 5) {
+				enemy[i].x = 5;
 			}
-		//}
+			else {
+				enemy[i].x = SCREEN_WIDTH - 5;
+			}
+			enemy[i].Enemy_angle = (1 - enemy[i].Enemy_angle) + 0.5f;
+			if (enemy[i].Enemy_angle > 1) enemy[i].Enemy_angle -= 1.0f;
+			ChangeAngle(enemy,i);
+		}
+
+		if (enemy[i].y < 5)                         //上の壁
+		{
+			enemy[i].Enemy_angle = (1 - enemy[i].Enemy_angle);
+			ChangeAngle(enemy,i);
+		}
+
+		//画面下を超えたらゲームオーバー
+		if (enemy[i].y > SCREEN_HIGHT + radius) {
+			ballState = BALL_STATE::IDLE;
+			}
+		}
+
+
+		if (ballState != BALL_STATE::IDLE) {
+			enemy[i].x += g_MoveX;
+			enemy[i].y += g_MoveY;
+		}
+		else {
+			enemy[i].x = bar.getX() + (bar.getWidth() / 2);
+			enemy[i].y = bar.getY() - ((bar.getHeight() + radius) / 2);
+		}
 	}
 }
 
@@ -101,6 +78,8 @@ void Enemy::CreateEnemy(Enemy* enemy) {
 			enemy[i].flg = true;
 			enemy[i].pos = GetRand(3);
 			GetEnemyPos(&enemy[i].x, &enemy[i].y, enemy[i].pos);
+			enemy[i].Enemy_angle = 0.625;
+			ChangeAngle(enemy, i);
 			enemy->Enemy_cnt++;
 		}
 	}
@@ -129,6 +108,12 @@ void Enemy::GetEnemyPos(float* enemy_x, int* enemy_y, int enemy_pos) {
 	}
 }
 
+void Enemy::ChangeAngle(Enemy* enemy, int num) {
+	float rad = enemy[num].Enemy_angle * (float)M_PI * 2;
+	Move_X = (int)(5 * cosf(rad));
+	Move_Y = (int)(5 * sinf(rad));
+}
+
 float Enemy::GetEnemyX() const{
 	return x;
 }
@@ -137,3 +122,72 @@ int Enemy::GetEnemyY() const {
 	return y;
 }
 
+
+
+////if (timer % 2) {
+		//	if (enemy[i].flg) {
+		//		
+		//		if ((enemy[i].x <= 20 || enemy[i].x >= 620) && !enemy[i].Spawn_flg) {
+		//			if (enemy[i].pos <= 1) {
+		//				enemy[i].x += 10;
+		//			}
+		//			else
+		//			{
+		//				enemy[i].x -= 10;
+		//			}
+		//		}
+		//		else {
+		//			enemy[i].Spawn_flg = true;
+		//		}
+
+		//		if (enemy[i].Enemy_time++ % 300) {
+		//			enemy[i].Enemy_vector = GetRand(3);
+		//		}
+		//		if (enemy[i].Spawn_flg) {
+		//			switch (enemy[i].Enemy_vector) {
+		//			case UP:
+		//				enemy[i].y -= 10;
+		//				break;
+		//			case DOWN:
+		//				enemy[i].y += 10;
+		//				break;
+		//			case RIGHT:
+		//				if (enemy[i].pos <= 1) {
+		//					enemy[i].x += 10;
+		//				}
+		//				else
+		//				{
+		//					enemy[i].x += 5;
+		//				}
+		//				break;
+		//			case LEFT:
+		//				if (enemy[i].pos <= 1) {
+		//					enemy[i].x -= 5;
+		//				}
+		//				else
+		//				{
+		//					enemy[i].x -= 10;
+		//				}
+		//				break;
+		//			default:
+		//				break;
+		//			}
+		//			/*if (Enemy_vector <= 1) {
+		//				enemy[i].x += (GetRand(2) - 1);
+		//			}
+		//			else {
+		//				enemy[i].y += (GetRand(2) - 1);
+		//			}*/
+		//		}
+
+		//		
+		//		
+
+		//		if (enemy[i].Spawn_flg && (enemy[i].x < -5 || enemy[i].x > 645 || enemy[i].y < -5 || enemy[i].y > 485)) {
+		//			enemy[i].flg = false;
+		//			enemy[i].Spawn_flg = false;
+		//			enemy[i].Enemy_time = 0;
+		//			enemy->Enemy_cnt--;
+		//		}
+		//	}
+		////}
--- Summer_Game_Jam/enemy.h
+++ Summer_Game_Jam/enemy.h
@@ -8,6 +8,9 @@ private:
 	float x;
 	int y;
 	int pos;
+	int Move_X;
+	int Move_Y;
+	float Enemy_angle;
 	int Enemy_cnt;
 	int Enemy_time;
 	int Enemy_vector;
@@ -17,6 +20,7 @@ public:
 	void DrawEnemy(int enemy_x,int enemy_y) const;		//蚊描画関数
 	void MoveEnemy(Enemy* enemy);		//蚊移動関数
 	void CreateEnemy(Enemy* enemy);		//蚊生成関数
+	void ChangeAngle(Enemy* enemy,int num);		//蚊角度変更関数
 	void GetEnemyPos(float* enemy_x, int* enemy_y, int enemy_pos);					//蚊出現位置決定関数
 	float GetEnemyX() const;
 	int GetEnemyY() const;
