0ea0f27b0dcab9c337704a01a87e64f986e5024e
Wan1129 <106945621+hiuhou-wan@users.noreply.github.com>
2022-07-25 08:55:06 +0900

    add CreateEnemy

---
+++ Summer_Game_Jam/enemy.cpp
@@ -0,0 +1,11 @@
+#include "DxLib.h"
+#include"enemy.h"
+
+void CreateEnemy() {
+	//ƒ‰ƒ“ƒ_ƒ€‚É•`‰æ
+	DrawCircle(GetRand(RandMaxX), GetRand(RandMaxY), 5, 0x000000, TRUE);
+}
+
+void EnemyControl() {
+
+}
