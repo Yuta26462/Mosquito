1645ed4898949e5df1f49ff5d08cb04073214d05
Yuta26462 <yuuta-miyagi-k21h@stu.kbc.ac.jp>
2022-07-25 14:20:20 +0900

    Revert "Revert "Change PlayerControl""
    This reverts commit 1443f76f28332997bfcc295a54f2e529ec3e7a0f.

--- Summer_Game_Jam/player.cpp
+++ Summer_Game_Jam/player.cpp
@@ -19,21 +19,23 @@ void Player_Finalize() {
 }
 void Player_Update() {
 	if (AttackFlg == false) {
-		if (g_KeyFlg & PAD_INPUT_RIGHT) {
+		if (g_KeyFlg & PAD_INPUT_RIGHT || GetSelectX() == 1) {
 			if (++BoxNumber_x > 2) BoxNumber_x = 2;
 		}
-		if (g_KeyFlg & PAD_INPUT_LEFT) {
+		if (g_KeyFlg & PAD_INPUT_LEFT || GetSelectX() == -1) {
 			if (--BoxNumber_x < 0) BoxNumber_x = 0;
 		}
-		if (g_KeyFlg & PAD_INPUT_DOWN) {
+		if (g_KeyFlg & PAD_INPUT_DOWN || GetSelectY() == 1) {
 			if (++BoxNumber_y > 1) BoxNumber_y = 1;
 		}
-		if (g_KeyFlg & PAD_INPUT_UP) {
+		if (g_KeyFlg & PAD_INPUT_UP || GetSelectY() == -1) {
 			if (--BoxNumber_y < 0) BoxNumber_y = 0;
 		}
-		if (g_KeyFlg & PAD_INPUT_A) {
+		if (g_KeyFlg & PAD_INPUT_A || g_KeyFlg & 32) {
 			AttackFlg = true;
 		}
+		if (GetJoyPadY() > 0 && GetJoyPadX() < 50 && GetJoyPadX() > -50) { BoxNumber_x = 1; BoxNumber_y = 1; }
+		else if((GetJoyPadY() < 0) && GetJoyPadY() < 50 && GetJoyPadY() > -50){BoxNumber_x = 1; BoxNumber_y = 0; }
 	}
 	if (AttackFlg == true) {
 		AttackCount++;
