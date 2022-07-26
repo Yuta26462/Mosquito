44a6b2b3f3896680d457cb6560811454fd348249
KBC12006077\kbc12006077 <hiroto-matayoshi-20h@stu.kbc.ac.jp>
2022-07-26 09:23:06 +0900

    ゲージの描画

--- Summer_Game_Jam/GameMain.cpp
+++ Summer_Game_Jam/GameMain.cpp
@@ -3,10 +3,12 @@
 #include "main.h"
 #include "DxLib.h"
 #include "player.h"
+#include "sleep.h"
 
 
 BaseScene* GameMain::Update() {
 	Player_Update();
+	Sleep_Update();
 	//zキーでリザルト画面へ移動
 	if (g_KeyFlg & PAD_INPUT_Y) {
 		return new Result();
@@ -17,14 +19,17 @@ BaseScene* GameMain::Update() {
 }
 void GameMain::Initialize() const {
 	Player_Initialize();
+	Sleep_Initialize();
 }
 
 void GameMain::Finalize() const {
 	Player_Finalize();
+	Sleep_Finalize();
 }
 void GameMain::Draw() const {
 	DrawGraph(0, 0, GameMain_img, FALSE);
 	DrawString(270, 220, "GameMain画面", 0x000000);
 	DrawString(250, 400, "Yボタンで次のシーンへ", 0x000000);
 	Player_Draw();
+	Sleep_Draw();
 }
--- Summer_Game_Jam/sleep.h
+++ Summer_Game_Jam/sleep.h
@@ -1,6 +1,11 @@
 #pragma once
 
-static int ImageSleep;
+static int ImageSleep;		//Q‚Ä‚é‰æ‘œ
+static int ImageSleep2;		//Q‚Ú‚¯‰æ‘œ
+static int gauge = 0, gaugeMax = 100;
+static int black = GetColor(0, 0, 0);
+static int blue = GetColor(0, 255, 255);
+static int red = GetColor(255, 255, 255);
 
 void Sleep_Initialize();
 void Sleep_Finalize();
