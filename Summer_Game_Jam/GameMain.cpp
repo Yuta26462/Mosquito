ecc70d87e9af379813881383da6a4fd4b806a0ea
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2022-07-26 13:49:49 +0900

    test

--- Summer_Game_Jam/GameMain.cpp
+++ Summer_Game_Jam/GameMain.cpp
@@ -6,10 +6,6 @@
 #include "player.h"
 #include "sleep.h"
 
-//GameMain::GameMain() {
-//	//enemy = 0;
-//	//Enemy_cnt = 0;
-//}
 
 BaseScene* GameMain::Update() {
 	static bool StartFlg = true;
@@ -17,12 +13,13 @@ BaseScene* GameMain::Update() {
 		enemy->InitEnemy(enemy);
 		StartFlg = false;
 	}
-	
-	enemy->MoveEnemy(enemy,TimeLimt);
+
+	enemy->MoveEnemy(enemy, TimeLimt);
+	enemy->CheckEnemyAlive(enemy);
 	Player_Update();
-	Sleep_Update();
-	//z?L?[????U???g??????
-	if (TimeLimt-- < 1 || g_KeyFlg & PAD_INPUT_Y) {
+	Sleep_Update(enemy);
+	//デバッグ用		Yボタンでリザルト画面へ
+	if (TimeLimt-- < 1 || g_KeyFlg & 128) {
 		return new Result();
 	}
 	else {
@@ -39,21 +36,19 @@ void GameMain::Finalize() const {
 	Sleep_Finalize();
 }
 
-void GameMain::Draw() const{
+void GameMain::Draw() const {
 	DrawGraph(0, 0, GameMain_img, FALSE);
-	for (int i = 0; i < 10; i++) {
-		enemy[i].DrawEnemy(enemy[i].GetEnemyX(),enemy[i].GetEnemyY(),enemy[i].GetEnemyFlg());
+	for (int i = 0; i < 20; i++) {
+		enemy->DrawEnemy(enemy[i]);
 	}
-	
+
+
 	DrawFormatString(100, 400, 0x000000, "Died_Enemy:%d", enemy->GetDied_enemy());
-	DrawFormatString(500, 100, 0x000000, "Time:%d", GetTime()/60);
-	DrawString(270, 220, "GameMain???", 0x000000);
-	DrawString(250, 400, "Y?{?^???????V?[????", 0x000000);
-	Player_Draw(); 
+	DrawFormatString(500, 100, 0x000000, "Time:%d", GetTime() / 60);
+	Player_Draw();
 	Sleep_Draw();
 }
 
-int GameMain::GetTime() const{
+int GameMain::GetTime() const {
 	return TimeLimt;
 }
-	
