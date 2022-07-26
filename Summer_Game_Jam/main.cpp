b399f903356f90022ad3f18350bc3e56eefc83c5
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2022-07-26 13:50:54 +0900

    test

--- Summer_Game_Jam/main.cpp
+++ Summer_Game_Jam/main.cpp
@@ -5,41 +5,41 @@
 #include<math.h>
 
 class Fps {
-	int mStartTime; //測定開始時刻
-	int mCount;  //カウンタ
-	float mFps;  //fps
-	static const int N = 60;  //平均を取るサンプル数
-	static const int FPS = 60;  //設定したFPS
+    int mStartTime; //測定開始時刻
+    int mCount;  //カウンタ
+    float mFps;  //fps
+    static const int N = 60;  //平均を取るサンプル数
+    static const int FPS = 60;  //設定したFPS
 
 public:
-	Fps() {
-		mStartTime = 0;
-		mCount = 0;
-		mFps = 0;
-	}
-	bool Update() {
-		if (mCount == 0) {//1フレーム目なら時刻を記憶
-			mStartTime = GetNowCount();
-		}
-		if (mCount == N) {
-			int t = GetNowCount();
-			mFps = 1000.f / ((t - mStartTime) / (float)N);
-			mCount = 0;
-			mStartTime = t;
-		}
-		mCount++;
-		return true;
-	}
-	void Draw() {
-		DrawFormatString(100, 100, GetColor(255, 255, 255), "%.1f", mFps);
-	}
-	void Wait() {
-		int tookTime = GetNowCount() - mStartTime;
-		int waitTime = mCount * 1000 / FPS - tookTime;
-		if (waitTime > 0) {
-			Sleep(waitTime);
-		}
-	}
+    Fps() {
+        mStartTime = 0;
+        mCount = 0;
+        mFps = 0;
+    }
+    bool Update() {
+        if (mCount == 0) {//1フレーム目なら時刻を記憶
+            mStartTime = GetNowCount();
+        }
+        if (mCount == N) {
+            int t = GetNowCount();
+            mFps = 1000.f / ((t - mStartTime) / (float)N);
+            mCount = 0;
+            mStartTime = t;
+        }
+        mCount++;
+        return true;
+    }
+    void Draw() {
+        //DrawFormatString(100, 100, GetColor(255, 255, 255), "%.1f", mFps);
+    }
+    void Wait() {
+        int tookTime = GetNowCount() - mStartTime;
+        int waitTime = mCount * 1000 / FPS - tookTime;
+        if (waitTime > 0) {
+            Sleep(waitTime);
+        }
+    }
 };
 
 int	g_OldKey;				// 前回の入力キー
@@ -59,18 +59,18 @@ int SelectX, SelectY;
 int LoadImages();           //画像読み込み関数
 
 int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	Fps fps;
+    Fps fps;
     SetOutApplicationLogValidFlag(FALSE);   //ログ出力を無効にする
     SetMainWindowText("蚊");
 
     ChangeWindowMode(TRUE); //ウィンドウモードを有効
     SetWindowSize(1280, 960);
-    
+
     if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
     if (LoadImages() == -1)return -1;
 
     SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
-    
+
     ChangeFontType(DX_FONTTYPE_ANTIALIASING_4X4);   //フォントをアンチエイジング対応4×4にする。
 
     SceneManager scenMG(new Title());
@@ -80,9 +80,10 @@ int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     while (scenMG.Update() != nullptr && ProcessMessage() == 0) {
         // 入力キー取得
         g_OldKey = g_NowKey;
-        g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-       // g_NowKey = GetJoypadInputState(DX_INPUT_PAD1);
+        g_NowKey = GetJoypadInputState(DX_INPUT_PAD1);
         g_KeyFlg = g_NowKey & ~g_OldKey;
+
+        if (g_KeyFlg & 1024) DxLib_End();   //BACKボタンでプログラム終了
 
         GetJoypadAnalogInput(&JoyPadX, &JoyPadY, DX_INPUT_PAD1);    //アナログスティック座標を取得
         SetJoypadDeadZone(DX_INPUT_PAD1, 0.3f);     //角度の制限を緩和
@@ -100,11 +101,11 @@ int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
         ClearDrawScreen();		// 画面の初期化
 
-		fps.Update();
-		fps.Draw();
+        fps.Update();
+        fps.Draw();
         scenMG.Draw();
 
-		fps.Wait();
+        fps.Wait();
         ScreenFlip();			// 裏画面の内容を表画面に反映
 
     }
@@ -135,14 +136,16 @@ int GetSelectY(void) {
     if (SelectY == 1 || SelectY == -1) {
         int Key = SelectY;
         return Key;
-    }else{ return 0; }
+    }
+    else { return 0; }
 }
 
-int GetJoyPadX(void) { 
+int GetJoyPadX(void) {
     if (-1000 <= JoyPadX && JoyPadX <= 1000) {
         int PadX = JoyPadX;
         return PadX;
-    }else { return 0; }
+    }
+    else { return 0; }
 }
 
 int GetJoyPadY(void) {
